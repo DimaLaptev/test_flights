886ec222f2625b3e13c9453f538a157aa0f6d7b0
shipilov-d <shipilenok1@gmail.com>
2022-04-21 21:27:36 +0300

    Initialisation DataBase on Postgres. Add gitignore.

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from sqlalchemy import create_engine, Integer, String
 from routes.flights import flights_bp
 
 
@@ -6,3 +7,8 @@ def create_app():
     app = Flask(__name__)
     app.register_blueprint(flights_bp)
     return app
+
+
+engine = create_engine('postgresql+psycopg2://postgres:1234@localhost/WelbeX_test', echo=True, future=True)
+engine.connect()
+print(engine)
