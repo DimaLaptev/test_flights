2fd028715d2db7ed228d8444906b0edb851f39bf
shipilov-d <shipilenok1@gmail.com>
2022-04-26 18:58:19 +0300

    add SESSION_MAKER in app.py-flights.py, add class Flight in Models/__init__.py, add "with app_context()" for flask.g

--- routes/flights.py
+++ routes/flights.py
@@ -1,18 +1,35 @@
-from flask import Flask, Blueprint, jsonify
+import flask
+from flask import Flask, Blueprint, jsonify, g, current_app
+from sqlalchemy import select
+from sqlalchemy.orm import Session, sessionmaker
+from models import Flight, Base
 
-app = Flask(__name__)
 flights_bp = Blueprint('flights', __name__, url_prefix='/api/flights')
 
 
 @flights_bp.route('/', methods=['GET'])
 def get_flights():
-    flights = [
-        {
-            'id': 1,
-            'date': u'22.06.1941',
-            'name': u'Moskow',
-            'count': u'100',
-            'distance': u'1300'
-        }
-    ]
-    return jsonify({'flights': flights})
+    with flask.current_app.app_context():
+        sm = flask.current_app.config.get('SESSION_MAKER')
+        session = sm()
+        print(g.get('engine', None))
+        stmt = select(Flight)
+        flights = session.scalars(stmt)
+        for flight in flights:
+            print(flight)
+    return jsonify({'data' : []})
+
+# with Session(g) as session:
+
+#    Moskow = Flight(
+#        date='2022-09-21',
+#        count='211',
+#        distance='2100.5',
+#        name='Moskow'
+#    )
+
+#    session.add(Moskow)
+
+#    session.commit()
+
+#
