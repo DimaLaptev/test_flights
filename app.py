ecc2dae95f8847b89d25e191d0778d126ac70c58
shipilov-d <shipilenok1@gmail.com>
2022-04-28 22:52:40 +0300

    Cleanup code

--- app.py
+++ app.py
@@ -8,9 +8,10 @@ from routes.flights import flights_bp
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(flights_bp)
+
     session_maker = sessionmaker(future=True)
     engine = create_engine('postgresql+psycopg2://postgres:1234@localhost/WelbeX_test', echo=True, future=True)
     session_maker.configure(bind=engine)
     app.config['SESSION_MAKER'] = session_maker
-    print(session_maker)
+
     return app
